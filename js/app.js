8dbf9b86b694beb899c6566d652c08c043f7ad2e
dale <hessdale96@gmail.com>
2023-03-15 13:55:48 -0600

    added age variable, is_subscribed and user_points made it tell using if else

---
+++ js/app.js
@@ -0,0 +1,17 @@
+let age = 18;
+let is_subscribed = true;
+let user_points = 100
+
+if (age < 18 && is_subscribed === false) {
+    console.log("The user is not 18 and not subscribed");
+} else if (age >= 18 && is_subscribed === false) {
+    console.log("The user is 18 and not subscribed");
+} else if (age < 18 && is_subscribed === true) {
+    console.log("The user is not 18 and is subscribed");
+} else if (age >= 18 && is_subscribed === true) {
+    console.log("The user is 18 and subscribed");
+}
+
+if ((age >= 18 && is_subscribed === true) && (user_points >= 100)) {
+    console.log("congrats on have more than 100 points being over 18 and being subscribed")
+} 
